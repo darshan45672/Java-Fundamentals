999f3df790246d4bff13c38ba2e0308d49e6d129
darshan45672 <drshnbhandary@gmail.com>
2023-06-09 11:32:51 +0530

    added comment

--- areaOfASquare.java
+++ areaOfASquare.java
@@ -1,6 +1,6 @@
 /* Java Procedural-Program to calculate area */
 
-import java.util.Scanner;
+import java.util.Scanner;  // 
 
 public class areaOfASquare {
     public static void main(String[] args) {
