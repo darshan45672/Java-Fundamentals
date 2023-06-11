0e7f4346b77f6917a9dd464b125ab0912bb77f70
darshan45672 <drshnbhandary@gmail.com>
2023-06-11 11:31:50 +0530

    File updated

--- areaOfASquare.java
+++ areaOfASquare.java
@@ -2,11 +2,14 @@
 
 import java.util.Scanner;  // header file that contias classes of scanner 
 
-public class areaOfASquare {  // creating 
-    public static void main(String[] args) {
-        System.out.println("Enter the Dimensions of a square: ");
-        Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
+public class areaOfASquare {  // creating a class small as file name
+
+    public static void main(String[] args) {  // main() of classs
+
+        System.out.println("Enter the Dimensions of a square: "); // taking input from the user uisng Sout
+        Scanner sc = new Scanner(System.in); // creating a object sc of class Scanner 
+
+        double a = sc.nextDouble();  //reading the user entered data from the console using the scanner object 
         System.out.println("Area of the square is "+ (a*a));
     }    
 }
