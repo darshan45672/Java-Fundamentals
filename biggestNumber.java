1387dd5d3a363110127951a10ca1b1ee40b4c8f3
darshan45672 <drshnbhandary@gmail.com>
2023-06-19 07:24:26 +0530

    new file biggestNumber.java added

---
+++ biggestNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class biggestNumber {
+    public static void main(String[] args) {
+        int big;
+        System.out.println("Enter the number of elements:\t");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        System.out.println("Enter "+ num +" elements:");
+        int a[] = new int[num];
+        for (int i = 0; i < num; i++) {
+            a[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < num; i++) {
+            big = a[0];
+            for (int j = 0; j < num; j++) {
+                if (a[i] > big)
+                    big = a[i];
+            } 
+        }
+        System.out.println("Biggest number entered is : " + big );
+    }
+}
