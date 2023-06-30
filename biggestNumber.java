97ec2f2ed296d6b3af805fdb07bcf7733476277b
darshan45672 <drshnbhandary@gmail.com>
2023-06-30 07:21:35 +0530

    New update

--- biggestNumber.java
+++ biggestNumber.java
@@ -7,6 +7,7 @@ public class biggestNumber {  // creating a public class biggestNumber
     public static void main(String[] args) {  //start of main
 
         int big; // declaring the variable
+        
         System.out.println("Enter the number of elements:\t");
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
