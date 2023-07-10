6e31e7e02c9f8d08be7bcfb136523b9184780200
darshan45672 <drshnbhandary@gmail.com>
2023-07-10 07:56:10 +0530

    New update

--- biggestNumber.java
+++ biggestNumber.java
@@ -22,7 +22,7 @@ public class biggestNumber {  // creating a public class biggestNumber
         // checking the biggest number
         for (int i = 0; i < num; i++) {
             big = a[0]; // assiging first array element as big
-            if (a[i] > big)
+            if (a[i] > big)  // 
                 big = a[i];
         }
         //System.out.println("Biggest number entered is : " + big );
