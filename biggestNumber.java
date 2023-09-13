dd79a655584f680d6cd6ce7bc1ff47cd55ed16f7
Darshan bhandary <109609462+darshan45672@users.noreply.github.com>
2023-09-13 20:41:42 +0530

    Update biggestNumber.java

--- biggestNumber.java
+++ biggestNumber.java
@@ -2,6 +2,7 @@
 
 import java.util.Scanner;  // header file that contians classes of scanner 
 
+
 public class biggestNumber {  // creating a public class biggestNumber
 
     public static void main(String[] args) {  //start of main
